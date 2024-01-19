575d0b6484a6198b008f32a0d3f0a5abd14ba528
steedude <85280987+steedude@users.noreply.github.com>
2024-01-19 16:16:14 +0800

    fix: internet test

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,12 @@
 import type { Express } from 'express';
 import client from './client';
 import admin from './admin';
+import type { Request, Response } from 'express';
 
 export default (app: Express) => {
   app.use('/api', client);
   app.use('/api/admin', admin);
+  app.get('/', (_: Request, res: Response) => {
+    res.status(200).send('連線成功');
+  });
 };
